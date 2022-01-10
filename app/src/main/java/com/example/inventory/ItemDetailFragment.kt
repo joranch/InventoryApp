8729cef0e8ee49f8274e734c35bc9e9a52ae8216
Jøran Haines <joran.haines@gmail.com>
2022-01-10 15:46:21 +0530

    add

---
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -0,0 +1,93 @@
+package com.example.inventory
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
+import com.example.inventory.databinding.FragmentItemDetailBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+/**
+ * [ItemDetailFragment] displays the details of the selected item.
+ */
+class ItemDetailFragment : Fragment() {
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    private var _binding: FragmentItemDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory((activity?.application as InventoryApplication).database.itemDao())
+    }
+
+    lateinit var item: Item
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bindViews(item)
+        }
+    }
+
+    private fun showConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(android.R.string.dialog_alert_title))
+            .setMessage(getString(R.string.delete_question))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.no)) { _, _ -> }
+            .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                deleteItem()
+            }
+            .show()
+    }
+
+    private fun deleteItem() {
+        viewModel.deleteItem(item)
+        findNavController().navigateUp()
+    }
+
+    private fun goToEditItem() {
+        val action = ItemDetailFragmentDirections.actionItemDetailFragmentToAddItemFragment(
+            getString(R.string.edit_fragment_title),
+            item.id)
+
+        findNavController().navigate(action)
+    }
+
+    private fun bindViews(item: Item) {
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.getFormattedPrice()
+            itemCount.text = item.quantityInStock.toString()
+
+            sellItem.setOnClickListener { viewModel.sellItem(item) }
+            sellItem.isEnabled = viewModel.isStockAvailable(item)
+            deleteItem.setOnClickListener { showConfirmationDialog() }
+            editItem.setOnClickListener { goToEditItem() }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
